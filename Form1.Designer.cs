aa1892c86ad409486af93f9a1af5ead4ef2b46f1
jonesfreedman <jonesfreedman@gmail.com>
2019-11-13 01:45:56 +0530

    Initial Commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace GameOfLife
+{
+    partial class GOL
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvcells = new System.Windows.Forms.DataGridView();
+            this.tmrspeed = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblgen = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvcells)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvcells
+            // 
+            this.dgvcells.AllowUserToAddRows = false;
+            this.dgvcells.AllowUserToDeleteRows = false;
+            this.dgvcells.AllowUserToResizeColumns = false;
+            this.dgvcells.AllowUserToResizeRows = false;
+            this.dgvcells.BackgroundColor = System.Drawing.Color.White;
+            this.dgvcells.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.dgvcells.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvcells.ColumnHeadersVisible = false;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.Black;
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dgvcells.DefaultCellStyle = dataGridViewCellStyle3;
+            this.dgvcells.GridColor = System.Drawing.Color.LightGray;
+            this.dgvcells.Location = new System.Drawing.Point(0, 44);
+            this.dgvcells.Name = "dgvcells";
+            this.dgvcells.ReadOnly = true;
+            this.dgvcells.RowHeadersVisible = false;
+            this.dgvcells.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.dgvcells.Size = new System.Drawing.Size(1018, 552);
+            this.dgvcells.TabIndex = 0;
+            // 
+            // tmrspeed
+            // 
+            this.tmrspeed.Interval = 1000;
+            this.tmrspeed.Tick += new System.EventHandler(this.tmrspeed_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.Black;
+            this.label1.Location = new System.Drawing.Point(12, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 21);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Generation";
+            // 
+            // lblgen
+            // 
+            this.lblgen.AutoSize = true;
+            this.lblgen.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblgen.ForeColor = System.Drawing.Color.Black;
+            this.lblgen.Location = new System.Drawing.Point(100, 11);
+            this.lblgen.Name = "lblgen";
+            this.lblgen.Size = new System.Drawing.Size(19, 21);
+            this.lblgen.TabIndex = 2;
+            this.lblgen.Text = "1";
+            // 
+            // GOL
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1002, 599);
+            this.Controls.Add(this.lblgen);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvcells);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "GOL";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Game Of Life";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvcells)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvcells;
+        private System.Windows.Forms.Timer tmrspeed;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblgen;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,271 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GameOfLife
+{
+    public partial class GOL : Form
+    {
+        int GridSize = 25;
+        int Neighbours = 0;
+        int Generation = 1;
+        DataTable dtBecomesGreen = new DataTable();
+        DataTable dtBecomesGray = new DataTable();
+        public GOL()
+        {
+            InitializeComponent();
+            dtBecomesGreen.Columns.Add("rowIndex");
+            dtBecomesGreen.Columns.Add("columnIndex");
+
+            dtBecomesGray.Columns.Add("rowIndex");
+            dtBecomesGray.Columns.Add("columnIndex");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            GenerateGrid();
+            InitializeGrid();
+            tmrspeed.Start();
+        }
+
+        private void GenerateGrid()
+        {
+            for (int mi = 0; mi < GridSize; mi++)
+            {
+                dgvcells.Columns.Add("", "");
+                dgvcells.Columns[mi].Width = 40;
+                dgvcells.Rows.Add();
+            }
+        }
+
+        private void InitializeGrid()
+        {
+            Random randomRow = new Random();
+            Random randomColumn = new Random();
+
+            Random minValue = new Random();
+            Random maxValue = new Random();
+
+            int rowIndex = 0;
+            int columnIndex = 0;
+            for (int mi = 0; mi < (GridSize * GridSize); mi++)
+            {
+
+                rowIndex = randomRow.Next(0, 12);
+                columnIndex = randomColumn.Next(0, 10);
+                dgvcells.Rows[rowIndex].Cells[columnIndex].Style.BackColor = Color.Green;
+                dgvcells.Rows[rowIndex].Cells[columnIndex].Style.SelectionBackColor = Color.Green;
+
+                rowIndex = randomRow.Next(10, 25);
+                columnIndex = randomColumn.Next(0, 25);
+                dgvcells.Rows[rowIndex].Cells[columnIndex].Style.BackColor = Color.Green;
+                dgvcells.Rows[rowIndex].Cells[columnIndex].Style.SelectionBackColor = Color.Green;
+
+                rowIndex = randomRow.Next(minValue.Next(0, 15), maxValue.Next(0, 15));
+                columnIndex = randomColumn.Next(minValue.Next(18, 24), maxValue.Next(20, 24));
+                dgvcells.Rows[rowIndex].Cells[columnIndex].Style.BackColor = Color.Green;
+                dgvcells.Rows[rowIndex].Cells[columnIndex].Style.SelectionBackColor = Color.Green;
+
+            }
+        }
+
+        private int ActiveCells()
+        {
+            int activeCells = 0;
+            for (int mi = 0; mi < GridSize; mi++)
+            {
+                activeCells += dgvcells.Rows.Cast<DataGridViewRow>().AsEnumerable().Count(r => r.Cells[mi].Style.BackColor == Color.Green);
+            }
+            return activeCells;
+        }
+
+        private void ProcessGrid()
+        {
+            for (int mi = 0; mi < dgvcells.Rows.Count; mi++)
+            {
+                for (int mj = 0; mj < GridSize; mj++)
+                {
+                    if (dgvcells.Rows[mi].Cells[mj].Style.BackColor == Color.Green)
+                    {
+                        var rowIndex = dgvcells.Rows[mi].Cells[mj].RowIndex;
+                        var columnIndex = dgvcells.Rows[mi].Cells[mj].ColumnIndex;
+                        ScanActive(rowIndex, columnIndex);
+                    }
+                    else
+                    {
+                        var rowIndex = dgvcells.Rows[mi].Cells[mj].RowIndex;
+                        var columnIndex = dgvcells.Rows[mi].Cells[mj].ColumnIndex;
+                        ScanInActive(rowIndex, columnIndex);
+                    }
+                }
+            }
+            GenerateNextPattern();
+        }
+
+        private void ScanActive(int RowIndex, int ColumnIndex)
+        {
+            Neighbours = 0;
+            ScanTop(RowIndex, ColumnIndex);
+            ScanTopRight(RowIndex, ColumnIndex);
+            ScanRight(RowIndex, ColumnIndex);
+            ScanBottomRight(RowIndex, ColumnIndex);
+            ScanBottom(RowIndex, ColumnIndex);
+            ScanBottomLeft(RowIndex, ColumnIndex);
+            ScanLeft(RowIndex, ColumnIndex);
+            ScanTopLeft(RowIndex, ColumnIndex);
+            if (Neighbours != 2 && Neighbours != 3)
+            {
+                dtBecomesGray.Rows.Add();
+                dtBecomesGray.Rows[dtBecomesGray.Rows.Count - 1]["rowIndex"] = RowIndex;
+                dtBecomesGray.Rows[dtBecomesGray.Rows.Count - 1]["columnIndex"] = ColumnIndex;
+            }
+        }
+
+        private void ScanInActive(int RowIndex, int ColumnIndex)
+        {
+            Neighbours = 0;
+            ScanTop(RowIndex, ColumnIndex);
+            ScanTopRight(RowIndex, ColumnIndex);
+            ScanRight(RowIndex, ColumnIndex);
+            ScanBottomRight(RowIndex, ColumnIndex);
+            ScanBottom(RowIndex, ColumnIndex);
+            ScanBottomLeft(RowIndex, ColumnIndex);
+            ScanLeft(RowIndex, ColumnIndex);
+            ScanTopLeft(RowIndex, ColumnIndex);
+            if (Neighbours == 3)
+            {
+                dtBecomesGreen.Rows.Add();
+                dtBecomesGreen.Rows[dtBecomesGreen.Rows.Count - 1]["rowIndex"] = RowIndex;
+                dtBecomesGreen.Rows[dtBecomesGreen.Rows.Count - 1]["columnIndex"] = ColumnIndex;
+            }
+        }
+
+        private void ScanTop(int RowIndex, int ColumnIndex)
+        {
+            if (RowIndex > 0)
+            {
+                var backColour = dgvcells.Rows[RowIndex - 1].Cells[ColumnIndex].Style.BackColor;
+                if (backColour.Name == "Green")
+                {
+                    Neighbours++;
+                }
+            }
+        }
+
+        private void ScanTopRight(int RowIndex, int ColumnIndex)
+        {
+            if (RowIndex > 0 && ColumnIndex < dgvcells.Columns.Count - 1)
+            {
+                var backColour = dgvcells.Rows[RowIndex - 1].Cells[ColumnIndex + 1].Style.BackColor;
+                if (backColour.Name == "Green")
+                {
+                    Neighbours++;
+                }
+            }
+        }
+
+        private void ScanRight(int RowIndex, int ColumnIndex)
+        {
+            if (ColumnIndex < dgvcells.Columns.Count - 1)
+            {
+                var backColour = dgvcells.Rows[RowIndex].Cells[ColumnIndex + 1].Style.BackColor;
+                if (backColour.Name == "Green")
+                {
+                    Neighbours++;
+                }
+            }
+        }
+
+        private void ScanBottomRight(int RowIndex, int ColumnIndex)
+        {
+            if (RowIndex < dgvcells.Rows.Count - 1 && ColumnIndex < dgvcells.Columns.Count - 1)
+            {
+                var backColour = dgvcells.Rows[RowIndex + 1].Cells[ColumnIndex + 1].Style.BackColor;
+                if (backColour.Name == "Green")
+                {
+                    Neighbours++;
+                }
+            }
+        }
+
+        private void ScanBottom(int RowIndex, int ColumnIndex)
+        {
+            if (RowIndex < dgvcells.Rows.Count - 1)
+            {
+                var backColour = dgvcells.Rows[RowIndex + 1].Cells[ColumnIndex].Style.BackColor;
+                if (backColour.Name == "Green")
+                {
+                    Neighbours++;
+                }
+            }
+        }
+
+        private void ScanBottomLeft(int RowIndex, int ColumnIndex)
+        {
+            if (RowIndex < dgvcells.Rows.Count - 1 && ColumnIndex > 0)
+            {
+                var backColour = dgvcells.Rows[RowIndex + 1].Cells[ColumnIndex - 1].Style.BackColor;
+                if (backColour.Name == "Green")
+                {
+                    Neighbours++;
+                }
+            }
+        }
+
+        private void ScanLeft(int RowIndex, int ColumnIndex)
+        {
+            if (ColumnIndex > 0)
+            {
+                var backColour = dgvcells.Rows[RowIndex].Cells[ColumnIndex - 1].Style.BackColor;
+                if (backColour.Name == "Green")
+                {
+                    Neighbours++;
+                }
+            }
+        }
+
+        private void ScanTopLeft(int RowIndex, int ColumnIndex)
+        {
+            if (RowIndex > 0 && ColumnIndex > 0)
+            {
+                var backColour = dgvcells.Rows[RowIndex - 1].Cells[ColumnIndex - 1].Style.BackColor;
+                if (backColour.Name == "Green")
+                {
+                    Neighbours++;
+                }
+            }
+        }
+
+        private void GenerateNextPattern()
+        {
+            for (int mi = 0; mi < dtBecomesGreen.Rows.Count; mi++)
+            {
+                dgvcells.Rows[Convert.ToInt32(dtBecomesGreen.Rows[mi]["rowIndex"].ToString())].Cells[Convert.ToInt32(dtBecomesGreen.Rows[mi]["columnIndex"].ToString())].Style.BackColor = Color.Green;
+                dgvcells.Rows[Convert.ToInt32(dtBecomesGreen.Rows[mi]["rowIndex"].ToString())].Cells[Convert.ToInt32(dtBecomesGreen.Rows[mi]["columnIndex"].ToString())].Style.SelectionBackColor = Color.Green;
+            }
+            for (int mi = 0; mi < dtBecomesGray.Rows.Count; mi++)
+            {
+                dgvcells.Rows[Convert.ToInt32(dtBecomesGray.Rows[mi]["rowIndex"].ToString())].Cells[Convert.ToInt32(dtBecomesGray.Rows[mi]["columnIndex"].ToString())].Style.BackColor = Color.White;
+                dgvcells.Rows[Convert.ToInt32(dtBecomesGray.Rows[mi]["rowIndex"].ToString())].Cells[Convert.ToInt32(dtBecomesGray.Rows[mi]["columnIndex"].ToString())].Style.SelectionBackColor = Color.White;
+            }
+            dtBecomesGray.Rows.Clear();
+            dtBecomesGreen.Rows.Clear();
+            Generation++;
+            lblgen.Text = Generation.ToString();
+        }
+
+        private void tmrspeed_Tick(object sender, EventArgs e)
+        {
+            if (ActiveCells() > 0)
+            {
+                ProcessGrid();
+            }
+        }
+    }
+}
